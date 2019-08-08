bd2c80f1cfa910fdffa5bd7bd92ac83838ba6464
dada99 <thedada99@gmail.com>
2019-08-08 13:48:07 +0800

    modify directory struture again

--- docker-swarm/create-docker-vm.sh
+++ docker-swarm/create-docker-vm.sh
@@ -1,1 +1,1 @@
-awk '{ if($1 !~ /^\[/ ) {split($2,res,"="); print $1" "res[2]}}' inventory-for-test|xargs sudo ./kvm-install-vm-1.sh
+awk '{ if($1 !~ /^\[/ ) {split($2,res,"="); print $1" "res[2]"\n"}}' inventory-k8s-new|xargs -n2 sudo ./kvm-install-vm-1.sh 
