2be837f3d2eb711733289746b1fdfe65e58f3f7f
dada99 <dada99@163.com>
2019-09-05 20:49:12 +0800

    update with setting in home lab

--- k8s-new-1.14/create-k8s-vm.sh
+++ k8s-new-1.14/create-k8s-vm.sh
@@ -1,2 +1,3 @@
 awk '{ if($1 !~ /^\[/ && $1 !~ /^ansible/ ) {split($2,res,"="); print $1}}' inventory-k8s-new|xargs -n1 sudo virsh undefine
 awk '{ if($1 !~ /^\[/ && $1 !~ /^ansible/ ) {split($2,res,"="); print $1" "res[2]"\n"}}' inventory-k8s-new|xargs -n2 sudo /home/dada99/workplace/kvm-install-vm/kvm-install-vm-1.sh 
+awk '{ if($1 !~ /^\[/ && $1 !~ /^ansible/ ) {split($2,res,"="); print $1" "res[2]"\n"}}' inventory-k8s-new|xargs -n2 sudo ./kvm-install-vm-1.sh 
